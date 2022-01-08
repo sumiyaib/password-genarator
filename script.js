8f615ec25d06955652360462100443d27df32c34
Sumiya Ibrahim <sumiyaib_101@hotmail.com>
2022-01-08 17:09:20 +0000

    notes on criteria on js template

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("clicked button");
+
+  // 1. Prompt the user for the password criteria
+  //   a. Password length 8 <128
+  //   b. Lowercase, uppercase, numbers, special characters
+  // 2. Validate the input
+  // 3. Generate password based on criteria
+  // 3. Display password on the page.
+
+  return "insert password here";
 }
 
 // Write password to the #password input
