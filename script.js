0b141cdb5f98cb5801da6c70b467305781cd920d
Sumiya Ibrahim <sumiyaib_101@hotmail.com>
2022-01-08 19:48:11 +0000

    add array

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ const includeNumbersElement = document.getElementById("includeNumbers");
 const includeSymbolsElement = document.getElementById("includeSymbols");
 const form = document.getElementById("passwordGeneratorForm");
 const passwordDisplay = document.getElementById("passwordDisplay");
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
+
+characterAmountNumber.addEventListener("input", syncCharacterAmount);
+characterAmountRange.addEventListener("input", syncCharacterAmount);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const characterAmount = characterAmountNumber.value;
+  const includeUppercase = includeUppercaseElement.checked;
+  const includeNumbers = includeNumbersElement.checked;
+  const includeSymbols = includeSymbolsElement.checked;
+  const password = generatePassword(
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+  passwordDisplay.innerText = password;
+});
