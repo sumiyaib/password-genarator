c8baf74f82b0ebbaca1dcdaeaa147d6cf96d39f0
Sumiya Ibrahim <sumiyaib_101@hotmail.com>
2022-01-08 19:47:21 +0000

    characteramount

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // Assignment Code
+const characterAmountRange = document.getElementById("characterAmountRange");
+const characterAmountNumber = document.getElementById("characterAmountNumber");
+const includeUppercaseElement = document.getElementById("includeUppercase");
+const includeNumbersElement = document.getElementById("includeNumbers");
+const includeSymbolsElement = document.getElementById("includeSymbols");
+const form = document.getElementById("passwordGeneratorForm");
+const passwordDisplay = document.getElementById("passwordDisplay");
